f803d430f051ebfd648dcbc3f9229eb66bb277eb
David <up202203777@edu.fe.up.pt>
2022-10-14 00:12:32 +0100

    POLL table statred - in production

--- EBD/schema.sql
+++ EBD/schema.sql
@@ -1,27 +1,27 @@
--- -- The citext module provides a case-insensitive character string type. 
--- -- Essentially, it internally calls lower when comparing values.
--- -- https://www.postgresql.org/docs/current/citext.html
--- CREATE EXTENSION IF NOT EXISTS citext;
+-- The citext module provides a case-insensitive character string type. 
+-- Essentially, it internally calls lower when comparing values.
+-- https://www.postgresql.org/docs/current/citext.html
+CREATE EXTENSION IF NOT EXISTS citext;
 
--- -- Lets us create UUIDs, instead of SERIAL ids
--- -- https://www.geeksforgeeks.org/postgresql-uuid-data-type/
--- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- Lets us create UUIDs, instead of SERIAL ids
+-- https://www.geeksforgeeks.org/postgresql-uuid-data-type/
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- DROP TABLE IF EXISTS registered_users;
--- create table registered_users(
--- 	user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
--- 	name VARCHAR default 'name' NOT NULL,
--- 	surename VARCHAR default 'family name' NOT NULL,
--- 	nickname VARCHAR UNIQUE NOT NULL,
--- 	password text default sha256('default_password') NOT NULL,
--- 	email citext UNIQUE NOT NULL,
--- 	birth_date date default (current_date - INTERVAL '18 YEAR') CHECK (birth_date <= (current_date - INTERVAL '18 YEAR')),
--- 	date_registered TIMESTAMP default current_timestamp NOT NULL,
--- 	last_seen TIMESTAMP,
--- 	url text UNIQUE,
--- 	status text,
--- 	is_admin BOOLEAN DEFAULT false NOT NULL	
--- );
+DROP TABLE IF EXISTS registered_users;
+create table registered_users(
+	user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+	name VARCHAR default 'name' NOT NULL,
+	surename VARCHAR default 'family name' NOT NULL,
+	nickname VARCHAR UNIQUE NOT NULL,
+	password text default sha256('default_password') NOT NULL,
+	email citext UNIQUE NOT NULL,
+	birth_date date default (current_date - INTERVAL '18 YEAR') CHECK (birth_date <= (current_date - INTERVAL '18 YEAR')),
+	date_registered TIMESTAMP default current_timestamp NOT NULL,
+	last_seen TIMESTAMP,
+	url text UNIQUE,
+	status text,
+	is_admin BOOLEAN DEFAULT false NOT NULL	
+);
 
 
 DROP TABLE IF EXISTS event;
@@ -44,4 +44,13 @@ create table event(
 										ON DELETE CASCADE
 										ON UPDATE CASCADE
 	
+);
+
+drop table if exists poll;
+create table poll(
+	title text default 'title' NOT NULL,
+	host_name text default 'host name?' NOT NULL,
+	question text DEFAULT 'questions?' NOT NULL,
+	starts_at TIMESTAMP default (CURRENT_TIMESTAMP) NOT NULL,
+	end_at TIMESTAMP default (CURRENT_TIMESTAMP + INTERVAL '1 DAY') NOT NULL
 );
