bbd2d46cf257319c49ae817ca8085e50a966c528
David <up202203777@edu.fe.up.pt>
2022-10-13 20:56:43 +0100

    insert into file added

--- EBD/schema.sql
+++ EBD/schema.sql
@@ -15,9 +15,10 @@ create table registered_users(
 	nickname VARCHAR UNIQUE NOT NULL,
 	password text default sha256('default_password') NOT NULL,
 	email citext UNIQUE NOT NULL,
-	dateRegistered TIMESTAMP default current_timestamp NOT NULL,
-	lastSeem TIMESTAMP,
+	birth_date date default(current_date - INTERVAL '18 YEAR') NOT NULL,
+	date_registered TIMESTAMP default current_timestamp NOT NULL,
+	last_seen TIMESTAMP,
 	url text UNIQUE,
 	status text,
-	isAdmin BOOLEAN DEFAULT false NOT NULL	
+	is_admin BOOLEAN DEFAULT false NOT NULL	
 );
