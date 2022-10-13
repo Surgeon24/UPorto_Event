8e4ec2c6f355aafd7f8a8278387ad447acc0ef99
David <up202203777@edu.fe.up.pt>
2022-10-13 21:42:36 +0100

    event table added (incomplete - many questions)

--- EBD/schema.sql
+++ EBD/schema.sql
@@ -15,10 +15,25 @@ create table registered_users(
 	nickname VARCHAR UNIQUE NOT NULL,
 	password text default sha256('default_password') NOT NULL,
 	email citext UNIQUE NOT NULL,
-	birth_date date default(current_date - INTERVAL '18 YEAR') NOT NULL,
+	birth_date date default (current_date - INTERVAL '18 YEAR') CHECK (birth_date <= (current_date - INTERVAL '18 YEAR')),
 	date_registered TIMESTAMP default current_timestamp NOT NULL,
 	last_seen TIMESTAMP,
 	url text UNIQUE,
 	status text,
 	is_admin BOOLEAN DEFAULT false NOT NULL	
+);
+
+
+DROP TABLE IF EXISTS event;
+create table event(
+	event_id SERIAL PRIMARY KEY,
+	event_name VARCHAR default 'default name' NOT NULL,
+	-- default (to_char(CURRENT_TIMESTAMP, 'DD Mon YYYY HH24:MI')) NEED TO CHOOSE - CONSULT WITH TEACHER
+	-- not sure about date format
+	event_date TIMESTAMP default (to_timestamp('05 Dec 2023 22:00', 'DD Mon YYYY HH24:MI')) NOT NULL,
+	-- only registered users can go
+	participant_id uuid,  -- FOREIGN KEY? taken from registered_users table
+	creator_id uuid,      -- FOREIGN KEY? taken from registered_users table
+	description text default('FEUP party') NOT NULL,
+	is_public BOOLEAN	  -- not sure of what it does
 );
