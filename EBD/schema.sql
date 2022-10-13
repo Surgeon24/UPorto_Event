5702170a2cb77ad888e2b7c5b010b1f2be442e6e
David <up202203777@edu.fe.up.pt>
2022-10-13 22:24:39 +0100

    foreign keys added between registered_users and event tables

--- EBD/schema.sql
+++ EBD/schema.sql
@@ -1,27 +1,27 @@
--- The citext module provides a case-insensitive character string type. 
--- Essentially, it internally calls lower when comparing values.
--- https://www.postgresql.org/docs/current/citext.html
-CREATE EXTENSION IF NOT EXISTS citext;
+-- -- The citext module provides a case-insensitive character string type. 
+-- -- Essentially, it internally calls lower when comparing values.
+-- -- https://www.postgresql.org/docs/current/citext.html
+-- CREATE EXTENSION IF NOT EXISTS citext;
 
--- Lets us create UUIDs, instead of SERIAL ids
--- https://www.geeksforgeeks.org/postgresql-uuid-data-type/
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- -- Lets us create UUIDs, instead of SERIAL ids
+-- -- https://www.geeksforgeeks.org/postgresql-uuid-data-type/
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS registered_users;
-create table registered_users(
-	user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-	name VARCHAR default 'name' NOT NULL,
-	surename VARCHAR default 'family name' NOT NULL,
-	nickname VARCHAR UNIQUE NOT NULL,
-	password text default sha256('default_password') NOT NULL,
-	email citext UNIQUE NOT NULL,
-	birth_date date default (current_date - INTERVAL '18 YEAR') CHECK (birth_date <= (current_date - INTERVAL '18 YEAR')),
-	date_registered TIMESTAMP default current_timestamp NOT NULL,
-	last_seen TIMESTAMP,
-	url text UNIQUE,
-	status text,
-	is_admin BOOLEAN DEFAULT false NOT NULL	
-);
+-- DROP TABLE IF EXISTS registered_users;
+-- create table registered_users(
+-- 	user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+-- 	name VARCHAR default 'name' NOT NULL,
+-- 	surename VARCHAR default 'family name' NOT NULL,
+-- 	nickname VARCHAR UNIQUE NOT NULL,
+-- 	password text default sha256('default_password') NOT NULL,
+-- 	email citext UNIQUE NOT NULL,
+-- 	birth_date date default (current_date - INTERVAL '18 YEAR') CHECK (birth_date <= (current_date - INTERVAL '18 YEAR')),
+-- 	date_registered TIMESTAMP default current_timestamp NOT NULL,
+-- 	last_seen TIMESTAMP,
+-- 	url text UNIQUE,
+-- 	status text,
+-- 	is_admin BOOLEAN DEFAULT false NOT NULL	
+-- );
 
 
 DROP TABLE IF EXISTS event;
@@ -35,5 +35,13 @@ create table event(
 	participant_id uuid,  -- FOREIGN KEY? taken from registered_users table
 	creator_id uuid,      -- FOREIGN KEY? taken from registered_users table
 	description text default('FEUP party') NOT NULL,
-	is_public BOOLEAN	  -- not sure of what it does
+	is_public BOOLEAN,	  -- not sure of what it does
+	
+	FOREIGN KEY (participant_id) REFERENCES registered_users (user_id)
+										ON DELETE CASCADE
+										ON UPDATE CASCADE,
+	FOREIGN KEY (creator_id) REFERENCES registered_users (user_id)
+										ON DELETE CASCADE
+										ON UPDATE CASCADE
+	
 );
