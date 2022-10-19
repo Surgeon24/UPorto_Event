dbbec7163e4bfb7653250706cef8db79a6f8ad11
David <up202203777@edu.fe.up.pt>
2022-10-19 18:59:58 +0100

    adding photos tables

--- EBD/schema.sql
+++ EBD/schema.sql
@@ -10,12 +10,19 @@ CREATE EXTENSION IF NOT EXISTS citext;
 -- https://www.geeksforgeeks.org/postgresql-uuid-data-type/
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Q - questions to the teacher
 
+drop table if exists notification;
+drop table IF EXISTS event_photo;
+drop table IF EXISTS user_photo;
+
+--!!! I should do the right order of dropping/creating
 
 DROP TABLE IF EXISTS event;
 
 DROP TABLE IF EXISTS registered_users;
 create table registered_users(
+	-- Q uuid - do we need it, is it ok?
 	user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
 	name VARCHAR default 'name' NOT NULL,
 	surename VARCHAR default 'family name' NOT NULL,
@@ -24,6 +31,7 @@ create table registered_users(
 	email citext UNIQUE NOT NULL,
 	birth_date date default (current_date - INTERVAL '18 YEAR') CHECK (birth_date <= (current_date - INTERVAL '18 YEAR')),
 	date_registered TIMESTAMP default current_timestamp NOT NULL,
+	-- Q last log in. Good?
 	last_seen TIMESTAMP,
 	url text UNIQUE,
 	status text,
@@ -40,7 +48,7 @@ create table event(
 	event_date TIMESTAMP default (to_timestamp('05 Dec 2023 22:00', 'DD Mon YYYY HH24:MI')) NOT NULL,
 	-- only registered users can go
 	participant_id uuid,  -- FOREIGN KEY? taken from registered_users table
-	creator_id uuid,      -- FOREIGN KEY? taken from registered_users table
+	creator_id uuid UNIQUE,  -- FOREIGN KEY? taken from registered_users table
 	description text default('FEUP party') NOT NULL,
 	is_public BOOLEAN,	  -- not sure of what it does
 	
@@ -82,12 +90,54 @@ CREATE TABLE rating(
 );
 
 
-drop table if exists notification;
+
 DROP TYPE IF EXISTS notification_type;
 CREATE TYPE notification_type AS ENUM ('Reminder', 'Report', 'Comment');
+
 CREATE TABLE notification(
 	notification_id SERIAL PRIMARY KEY,
+	user_id uuid,
 	description text,
 	notification_date timestamp default current_timestamp,
-	type notification_type
+	type notification_type,
+
+	FOREIGN KEY (user_id) REFERENCES registered_users (user_id)
+										ON DELETE CASCADE
+										ON UPDATE CASCADE
+);
+
+
+
+
+
+-- Q should we have two table for event and user photos each?
+
+
+
+create table user_photo(
+	user_photo_id SERIAL,
+	file bytea,
+	added_on timestamp default current_timestamp,
+	added_by uuid,
+
+	-- Q should we have 'image_path' row?
+	
+	FOREIGN KEY (added_by) REFERENCES registered_users (user_id)
+																		ON DELETE CASCADE
+																		ON UPDATE CASCADE
+	);
+
+
+
+
+create table event_photo(
+	event_photo_id SERIAL,
+	file bytea,
+	added_on timestamp default current_timestamp,
+	-- creator
+	added_by uuid,
+
+	FOREIGN KEY (added_by) REFERENCES event (creator_id)
+									ON DELETE CASCADE
+									ON UPDATE CASCADE
 );
