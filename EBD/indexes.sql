d76bf3c9cacfc798d0d39202ee05d5b70eef7ed7
ValterCastro <up201706546@fe.up.pt>
2022-10-28 15:02:07 +0100

    Performance and FTS Indexes

--- EBD/indexes.sql
+++ EBD/indexes.sql
@@ -1,21 +1,12 @@
-CREATE INDEX nickname_registeredUsers ON registered_users USING hash (nickname);
-CREATE INDEX eventName_event ON event USING hash (event_name);
-CREATE INDEX tag_event ON event USING hash (tag);
-CREATE INDEX publishDate_comment ON comment USING hash (publish_date);
 
---hash 
+--PERFORMANCE INDEXES
 
-CREATE INDEX dateRegistered_resgisteredUsers ON registered_users USING btree (date_registered);
-CREATE INDEX eventDate_event ON event USING btree (event_date);
-CREATE INDEX startsAt_poll ON poll USING btree (starts_at);
-CREATE INDEX endAt_poll ON poll USING btree (end_at);
-CREATE INDEX notificationDate_notification_type ON notification_type USING btree (notification_date);
-CREATE INDEX addedOn_userPhoto ON user_photo USING btree (added_on);
-CREATE INDEX addedOn_eventPhoto ON event_photo USING btree (added_on);
+CREATE INDEX idx_event_name ON Event USING HASH (name);
+CREATE INDEX idx_event_startDate ON Event USING BTREE (start_date);
+CREATE INDEX idx_poll_startAt ON Event USING BTREE (starts_at);
 
+--FULL TEXT SEARCH INDEXES
 
---btree
-
---CREATE INDEX date-registered_resgistered-users ON registered_users USING btree (date_registered);
-
---gist
+CREATE INDEX idx_tag_name ON Tag USING GIST (name);
+CREATE INDEX idx_poll_title ON Poll USING GIST (title);
+CREATE INDEX idx_event_location ON Event USING GIST (location);
