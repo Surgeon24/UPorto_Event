2d1bbf8cc16d1c5ee93d93e5e992214be673ed10
ValterCastro <up201706546@fe.up.pt>
2022-10-28 11:28:52 +0100

    Syntax Fix

--- EBD/schema1.sql
+++ EBD/schema1.sql
@@ -1,13 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 DROP TABLE IF EXISTS RegisteredUser;
 DROP TABLE IF EXISTS Administrator;
-DROP TABLE IF EXISTS Event;
+DROP TABLE IF EXISTS Event CASCADE;
 DROP TABLE IF EXISTS Report;
 DROP TABLE IF EXISTS Invite;
 DROP TABLE IF EXISTS Comment;
-DROP TABLE IF EXISTS Notification;
+DROP TABLE IF EXISTS Notification CASCADE;
 DROP TABLE IF EXISTS ReportNotification;
 DROP TABLE IF EXISTS PollNotification;
 DROP TABLE IF EXISTS EventNotification;
@@ -17,57 +16,56 @@ DROP TABLE IF EXISTS Photo;
 DROP TABLE IF EXISTS Poll;
 DROP TABLE IF EXISTS PollOption;
 DROP TABLE IF EXISTS PollVote;
-
-CREATE TYPE STATUS AS enum ('Going', 'Maybe', 'Cant');
+CREATE TYPE REPORT_STATUS AS enum ('Going', 'Maybe', 'Cant');
 CREATE TYPE MEMBERROLE AS enum ('Owner', 'Moderator', 'Participant');
-
-
 CREATE TABLE IF NOT EXISTS RegisteredUser(
     ID uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-	name TEXT DEFAULT 'name' NOT NULL,
-	surename TEXT DEFAULT 'family name' NOT NULL,
-	nickname TEXT UNIQUE NOT NULL,
-	password TEXT DEFAULT sha256('DEFAULT_password') NOT NULL,
-	email citext UNIQUE NOT NULL,
-	date_registered TIMESTAMP DEFAULT current_timestamp NOT NULL,
-	last_seen TIMESTAMP,
-	birth_date DATE DEFAULT (current_date - INTERVAL '18 YEAR') CHECK LowerOrEqualThanCurrentDatePlus18 (birth_date <= (current_date - INTERVAL '18 YEAR')),
-	url URL UNIQUE,
-	status TEXT,
-	is_admin BOOLEAN DEFAULT false NOT NULL,
-	photo_path TEXT	
-
+    name TEXT DEFAULT 'name' NOT NULL,
+    surename TEXT DEFAULT 'family name' NOT NULL,
+    nickname TEXT UNIQUE NOT NULL,
+    password TEXT DEFAULT sha256('DEFAULT_password') NOT NULL,
+    email citext UNIQUE NOT NULL,
+    date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    last_seen TIMESTAMP,
+    birth_date DATE DEFAULT (current_date - INTERVAL '18 YEAR') CHECK (
+        birth_date <= (current_date - INTERVAL '18 YEAR')
+    ),
+    url TEXT UNIQUE,
+    status TEXT,
+    is_admin BOOLEAN DEFAULT false NOT NULL,
+    photo_path TEXT
 );
 CREATE TABLE IF NOT EXISTS Administrator(
     ID SERIAL PRIMARY KEY,
-    FOREIGN KEY (userID) REFERENCES RegisteredUser(ID)
+    adminID uuid DEFAULT uuid_generate_v4 (),
+    FOREIGN KEY (adminID) REFERENCES RegisteredUser(ID)
 );
 CREATE TABLE IF NOT EXISTS Event(
     ID SERIAL PRIMARY KEY,
-	name TEXT DEFAULT 'DEFAULT name' NOT NULL,
-	description TEXT DEFAULT('FEUP party') NOT NULL,
-	start_date TIMESTAMP DEFAULT (to_timestamp('05 Dec 2023 22:00', 'DD Mon YYYY HH24:MI')) NOT NULL,
-	is_public BOOLEAN DEFAULT True NOT NULL,
-	location TEXT DEFAULT 'Adega Leonor' NOT NULL,
-	schedule TEXT ,
+    name TEXT DEFAULT 'DEFAULT name' NOT NULL,
+    description TEXT DEFAULT('FEUP party') NOT NULL,
+    start_date TIMESTAMP DEFAULT (
+        to_timestamp('05 Dec 2023 22:00', 'DD Mon YYYY HH24:MI')
+    ) NOT NULL,
+    is_public BOOLEAN DEFAULT TRUE NOT NULL,
+    location TEXT DEFAULT 'Adega Leonor' NOT NULL,
+    schedule TEXT,
     role MEMBERROLE
-
 );
 CREATE TABLE IF NOT EXISTS Report(
     ID SERIAL PRIMARY KEY,
-    reportedID SERIAL PRIMARY KEY,
-    reporterID SERIAL ,
-    adminID SERIAL ,
+    reportedID uuid DEFAULT uuid_generate_v4 (),
+    reporterID uuid DEFAULT uuid_generate_v4 (),
+    adminID SERIAL,
     text TEXT NOT NULL,
     report_status STATUS,
-
     FOREIGN KEY (reportedID) REFERENCES RegisteredUser(ID),
     FOREIGN KEY (reporterID) REFERENCES RegisteredUser(ID),
-    FOREIGN KEY (adminID) REFERENCES Administrator(ID),
+    FOREIGN KEY (adminID) REFERENCES Administrator(ID)
 );
 CREATE TABLE IF NOT EXISTS Invite(
     ID SERIAL PRIMARY KEY,
-    userID SERIAL,
+    userID uuid DEFAULT uuid_generate_v4 (),
     eventID SERIAL,
     accepted BOOLEAN,
     FOREIGN KEY (userID) REFERENCES RegisteredUser(ID),
@@ -75,8 +73,8 @@ CREATE TABLE IF NOT EXISTS Invite(
 );
 CREATE TABLE IF NOT EXISTS Comment(
     ID SERIAL PRIMARY KEY,
-    publish_date DATE DEFAULT (current_date) CHECK LowerOrEqualThanCurrentDate (publish_date <= current_date),
-    userID SERIAL,
+    publish_date DATE DEFAULT (current_date) CHECK (publish_date <= current_date),
+    userID uuid DEFAULT uuid_generate_v4 (),
     eventID SERIAL,
     commentID SERIAL,
     FOREIGN KEY (userID) REFERENCES RegisteredUser(ID),
@@ -87,7 +85,7 @@ CREATE TABLE IF NOT EXISTS Notification(
     ID SERIAL PRIMARY KEY,
     text TEXT NOT NULL,
     date DATE NOT NULL,
-    userID SERIAL,
+    userID uuid DEFAULT uuid_generate_v4 (),
     FOREIGN KEY (userID) REFERENCES RegisteredUser(ID)
 );
 CREATE TABLE IF NOT EXISTS ReportNotification(
@@ -115,7 +113,7 @@ CREATE TABLE IF NOT EXISTS Tag(
 );
 CREATE TABLE IF NOT EXISTS Photo(
     ID SERIAL PRIMARY KEY,
-    upload_date DATE DEFAULT (current_date) ,
+    upload_date DATE DEFAULT (current_date),
     image_path TEXT UNIQUE,
     eventID SERIAL,
     FOREIGN KEY (eventID) REFERENCES Event(ID)
@@ -124,9 +122,9 @@ CREATE TABLE IF NOT EXISTS Poll(
     ID SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
-    starts_at DATE DEFAULT (current_date) CHECK LowerThanStartsAt (ends_at < current_date),
-    ends_at DATE DEFAULT (starts_at + INTERVAL '1 DAY'),
-    userID SERIAL,
+    starts_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP check(starts_at <= CURRENT_TIMESTAMP) NOT NULL,
+    end_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP + INTERVAL '1 DAY') check(end_at > starts_at) NOT NULL,
+    userID uuid DEFAULT uuid_generate_v4 (),
     eventID SERIAL,
     FOREIGN KEY (eventID) REFERENCES Event(ID),
     FOREIGN KEY (userID) REFERENCES RegisteredUser(ID)
@@ -139,10 +137,10 @@ CREATE TABLE IF NOT EXISTS PollOption(
 );
 CREATE TABLE IF NOT EXISTS PollVote(
     voteID SERIAL PRIMARY KEY,
-    userID SERIAL,
+    userID uuid DEFAULT uuid_generate_v4 (),
     optionID SERIAL,
     date DATE NOT NULL,
     FOREIGN KEY (voteID) REFERENCES Poll(ID),
-    FOREIGN KEY (userID) REFERENCES RegisteredUser(ID)
+    FOREIGN KEY (userID) REFERENCES RegisteredUser(ID),
     FOREIGN KEY (optionID) REFERENCES PollOption(ID)
 );
