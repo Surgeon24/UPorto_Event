167bd9376e70fff3629be0d50c9033dcb81d4875
ValterCastro <up201706546@fe.up.pt>
2022-10-31 15:12:30 +0000

    Removed poll index

--- EBD/indexes.sql
+++ EBD/indexes.sql
@@ -1,15 +1,9 @@
 
 --PERFORMANCE INDEXES
 
-<<<<<<< HEAD
 CREATE INDEX idx_event_name ON event USING HASH (name);
 CREATE INDEX idx_event_startDate ON event USING BTREE (start_date);
 
-=======
-CREATE INDEX IF NOT EXISTS idx_event_name ON event USING HASH (name);
-CREATE INDEX IF NOT EXISTS idx_event_start_date ON event USING BTREE (start_date);
-CREATE INDEX IF NOT EXISTS idx_poll_start_at ON Poll USING BTREE (starts_at);
->>>>>>> 54dbd6f2b854292c42e532cfa16853b8d14beb5b
 
 --FULL TEXT SEARCH INDEXES
 
