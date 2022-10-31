e7838352010d83219f29adf6767aeb20d10208c6
Mikhail Ermolaev <up202203498@fe.up.pt>
2022-10-31 13:46:36 +0000

    ebd and indexes have been changed

--- EBD/indexes.sql
+++ EBD/indexes.sql
@@ -1,12 +1,12 @@
 
 --PERFORMANCE INDEXES
 
-CREATE INDEX idx_event_name ON event USING HASH (name);
-CREATE INDEX idx_event_startDate ON event USING BTREE (start_date);
-CREATE INDEX idx_poll_startAt ON Poll USING BTREE (starts_at);
+CREATE INDEX IF NOT EXISTS idx_event_name ON event USING HASH (name);
+CREATE INDEX IF NOT EXISTS idx_event_start_date ON event USING BTREE (start_date);
+CREATE INDEX IF NOT EXISTS idx_poll_start_at ON Poll USING BTREE (starts_at);
 
 --FULL TEXT SEARCH INDEXES
 
-CREATE INDEX idx_tag_name ON tag USING GIST (name);
-CREATE INDEX idx_user_name ON authorized_user USING GIST (name);
-CREATE INDEX idx_event_location ON event USING GIST (location);
+CREATE INDEX IF NOT EXISTS idx_tag_name ON tag USING GIST (name);
+CREATE INDEX IF NOT EXISTS idx_user_name ON authorized_user USING GIST (name);
+CREATE INDEX IF NOT EXISTS idx_event_location ON event USING GIST (location);
