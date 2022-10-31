a183bcd5e171505be5ac43ee5e3547d69ffa638a
David <up202203777@edu.fe.up.pt>
2022-10-31 03:05:33 +0000

    poll, event, comment triggers

--- EBD/schemaA6.sql
+++ EBD/schemaA6.sql
@@ -37,6 +37,9 @@ CREATE TYPE REPORT_STATUS AS ENUM('Going', 'Maybe', 'Cant');
 drop type if exists MEMBER_ROLE;
 CREATE TYPE MEMBER_ROLE AS ENUM('Owner', 'Moderator', 'Participant');
 
+drop type if exists TYPE_NOTIFICATION;
+CREATE TYPE TYPE_NOTIFICATION AS ENUM('comment', 'event', 'poll', 'report');
+
 
 -----------------------------------------
 -- TABLES
@@ -86,7 +89,7 @@ CREATE TABLE IF NOT EXISTS report(
     id SERIAL PRIMARY KEY,
     reported_id uuid,
     reporter_id uuid,
-    admin_id SERIAL,
+    admin_id INT,
     text TEXT NOT NULL,
     report_status REPORT_STATUS,
     FOREIGN KEY (reported_id) REFERENCES registered_user(id),
@@ -99,7 +102,7 @@ CREATE TABLE IF NOT EXISTS report(
 CREATE TABLE IF NOT EXISTS user_event(
     id SERIAL PRIMARY KEY,
     user_id uuid,
-    event_id SERIAL,  
+    event_id INT,  
     role MEMBER_ROLE,
     accepted BOOLEAN,   -- used only in private events
     UNIQUE (user_id, event_id),  -- combination of user_id and event_id is UNIQUE because user can be registered at the event only once
@@ -126,8 +129,9 @@ CREATE TABLE IF NOT EXISTS comments(
 CREATE TABLE IF NOT EXISTS notification(
     id SERIAL PRIMARY KEY,
 		user_id uuid,
+		notification_type type_notification NOT NULL,
     notification_text TEXT NOT NULL DEFAULT ('text'),
-    notification_date DATE NOT NULL DEFAULT (current_date) CHECK (notification_date <= current_date) ,
+    notification_date DATE NOT NULL DEFAULT (current_date) CHECK (notification_date <= current_date),
     FOREIGN KEY (user_id) REFERENCES registered_user(id)
 );
 
@@ -136,7 +140,7 @@ CREATE TABLE IF NOT EXISTS tag(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     color TEXT NOT NULL,
-    event_id SERIAL,
+    event_id INT,
     FOREIGN KEY (event_id) REFERENCES event(id)
 );
 
@@ -145,7 +149,7 @@ CREATE TABLE IF NOT EXISTS photo(
     id SERIAL PRIMARY KEY,
     upload_date DATE DEFAULT (current_date),
     image_path TEXT UNIQUE,
-    event_id SERIAL,
+    event_id INT,
     FOREIGN KEY (event_id) REFERENCES event(id)
 );
 
@@ -156,17 +160,15 @@ CREATE TABLE IF NOT EXISTS poll(
     content TEXT NOT NULL,
     starts_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP check(starts_at <= CURRENT_TIMESTAMP) NOT NULL,
     end_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP + INTERVAL '1 DAY') check(end_at > starts_at) NOT NULL,
-    user_id uuid,
-    event_id SERIAL,
-    FOREIGN KEY (event_id) REFERENCES event(id),
-    FOREIGN KEY (user_id) REFERENCES registered_user(id)
+    event_id INT,
+    FOREIGN KEY (event_id) REFERENCES event(id)
 );
 
 
 CREATE TABLE IF NOT EXISTS poll_option(
     id SERIAL PRIMARY KEY,
     option TEXT NOT NULL,
-    poll_id SERIAL,
+    poll_id INT,
     FOREIGN KEY (poll_id) REFERENCES poll(id)
 );
 
@@ -174,7 +176,7 @@ CREATE TABLE IF NOT EXISTS poll_option(
 CREATE TABLE IF NOT EXISTS poll_vote(
     vote_id SERIAL PRIMARY KEY,
     user_id uuid,
-    option_id SERIAL,
+    option_id INT,
     date DATE NOT NULL,
     FOREIGN KEY (vote_id) REFERENCES poll(id),
     FOREIGN KEY (user_id) REFERENCES registered_user(id),
@@ -185,13 +187,13 @@ CREATE TABLE IF NOT EXISTS poll_vote(
 -- TRIGGERS
 -----------------------------------------
 
-CREATE or REPLACE FUNCTION comment_notification() RETURNS trigger AS $comment_notification$
+CREATE OR REPLACE FUNCTION comment_notification() RETURNS trigger AS $comment_notification$
 		BEGIN
 		IF (NEW.parent_comment_id IS NOT NULL)
 			THEN
 				INSERT INTO 
-							notification(notification_text,notification_date, user_id)
-							select NEW.comment_text, NEW.comment_date, user_id from comments where NEW.parent_comment_id = id;
+							notification(notification_text,notification_date, notification_type, user_id)
+							select NEW.comment_text, NEW.comment_date, 'comment' ,user_id from comments where NEW.parent_comment_id = id;
 			 END IF;
 							RETURN new;
 END;
@@ -201,19 +203,52 @@ language plpgsql;
 DROP TRIGGER IF EXISTS trig_comment ON public.comments;
 
 CREATE TRIGGER trig_comment
-     AFTER INSERT ON comments
+     AFTER INSERT OR UPDATE ON comments
      FOR EACH ROW
      EXECUTE PROCEDURE comment_notification();
 		 
 		 
 		 
 		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
+CREATE OR REPLACE FUNCTION event_notification() RETURNS trigger AS $event_notification$
+		BEGIN
+				INSERT INTO 
+							notification(notification_text,notification_date, notification_type, user_id)
+							VALUES('Welcome new participant!', CURRENT_TIMESTAMP, 'event', NEW.user_id);
+							RETURN new;
+END;
+$event_notification$
+language plpgsql;
+				
+DROP TRIGGER IF EXISTS trig_event ON public.user_event;
 
+CREATE TRIGGER trig_event
+     AFTER INSERT OR UPDATE ON user_event
+     FOR EACH ROW
+     EXECUTE PROCEDURE event_notification();
+		 
+		 
+		 
+CREATE OR REPLACE FUNCTION poll_notification() RETURNS trigger AS $poll_notification$
+		BEGIN
+				INSERT INTO 
+							notification(notification_date, notification_type, user_id, notification_text)
+							select NEW.starts_at, 'poll', NEW.user_id, 'New poll was created. ' || NEW.title || ' You can vote!' from poll;
+							RETURN new;
+END;
+$poll_notification$
+language plpgsql;
+				
+DROP TRIGGER IF EXISTS trig_poll ON public.poll;
+
+CREATE TRIGGER trig_poll
+     AFTER INSERT ON poll
+     FOR EACH ROW
+     EXECUTE PROCEDURE poll_notification();
+
+
+
+
+
+		 
+		 
