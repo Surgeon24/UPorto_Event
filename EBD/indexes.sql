f480db23b3ba96a8ec57ec1b5dcf3325bfa92eff
ValterCastro <up201706546@fe.up.pt>
2022-10-31 12:08:41 +0000

    Indexes Fixes

--- EBD/indexes.sql
+++ EBD/indexes.sql
@@ -1,12 +1,12 @@
 
 --PERFORMANCE INDEXES
 
-CREATE INDEX idx_event_name ON Event USING HASH (name);
-CREATE INDEX idx_event_startDate ON Event USING BTREE (start_date);
+CREATE INDEX idx_event_name ON event USING HASH (name);
+CREATE INDEX idx_event_startDate ON event USING BTREE (start_date);
 CREATE INDEX idx_poll_startAt ON Poll USING BTREE (starts_at);
 
 --FULL TEXT SEARCH INDEXES
 
-CREATE INDEX idx_tag_name ON Tag USING GIST (name);
-CREATE INDEX idx_poll_title ON Poll USING GIST (title);
-CREATE INDEX idx_event_location ON Event USING GIST (location);
+CREATE INDEX idx_tag_name ON tag USING GIST (name);
+CREATE INDEX idx_user_name ON authorized_user USING GIST (name);
+CREATE INDEX idx_event_location ON event USING GIST (location);
