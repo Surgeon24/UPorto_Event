682ce3da15321ad981c5b61f35869061a562804d
David Burchakov <up202203777@fe.up.pt>
2022-11-22 13:33:00 +0000

    Only tables

--- Docs/EBD/schemaA5.sql
+++ Docs/EBD/schemaA5.sql
@@ -6,7 +6,7 @@
 -- Essentially, it internally calls lower when comparing values.
 -- https://www.postgresql.org/docs/current/citext.html
 CREATE EXTENSION IF NOT EXISTS citext;
-
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -----------------------------------------
 -- DROPPING TABLES
@@ -18,22 +18,29 @@ DROP TABLE IF EXISTS event CASCADE;
 DROP TABLE IF EXISTS report CASCADE;
 DROP TABLE IF EXISTS user_event CASCADE;
 DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS notification CASCADE;
 DROP TABLE IF EXISTS tag CASCADE;
 DROP TABLE IF EXISTS photo CASCADE;
 DROP TABLE IF EXISTS poll CASCADE;
-DROP TABLE IF EXISTS poll_option CASCADE;
+DROP TABLE IF EXISTS poll_choice CASCADE;
 DROP TABLE IF EXISTS poll_vote CASCADE;
-
+DROP TABLE IF EXISTS event_notification CASCADE;
+DROP TABLE IF EXISTS comment_notification CASCADE;
+DROP TABLE IF EXISTS poll_notification CASCADE;
+DROP TABLE IF EXISTS report_notification CASCADE;
+DROP TABLE IF EXISTS notification CASCADE;
 
 -----------------------------------------
 -- TYPES
 -----------------------------------------
+drop type if exists REPORT_TYPE;
+CREATE TYPE REPORT_TYPE AS ENUM('Spam', 'Nudity or sexual activity', 'Hate speech or symbols', 'Violence or dangerous organisations', 'Bullying or harassment', 'Selling illegal or regulated goods', 'Scams or fraud', 'False information', 'Other');
+
 drop type if exists REPORT_STATUS;
-CREATE TYPE REPORT_STATUS AS ENUM('Spam', 'Nudity or sexual activity', 'Hate speech or symbols', 'Violence or dangerous organisations', 'Bullying or harassment', 'Selling illegal or regulated goods', 'Scams or fraud', 'False information');
+CREATE TYPE REPORT_STATUS AS ENUM('Waiting', 'Ignored', 'Sanctioned');
 
 drop type if exists MEMBER_ROLE;
 CREATE TYPE MEMBER_ROLE AS ENUM('Owner', 'Moderator', 'Participant');
+
 
 drop type if exists TYPE_NOTIFICATION;
 CREATE TYPE TYPE_NOTIFICATION AS ENUM('comment', 'event', 'poll', 'report');
@@ -45,13 +52,12 @@ CREATE TYPE TYPE_NOTIFICATION AS ENUM('comment', 'event', 'poll', 'report');
 
 CREATE TABLE IF NOT EXISTS authorized_user(
     ID uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    name TEXT DEFAULT 'name' NOT NULL,
-    surename TEXT DEFAULT 'family name' NOT NULL,
-    nickname TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    firstname TEXT DEFAULT 'Válter Ochôa de Spínola Catanho' NOT NULL,
+    lastname TEXT DEFAULT 'Castro' NOT NULL,
     password TEXT DEFAULT sha256('DEFAULT_password') NOT NULL,
     email citext UNIQUE NOT NULL,
     date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    last_seen TIMESTAMP,
     birth_date DATE DEFAULT (current_date - INTERVAL '18 YEAR') CHECK (
         birth_date <= (current_date - INTERVAL '18 YEAR')
     ),
@@ -73,7 +79,7 @@ CREATE TABLE IF NOT EXISTS administrator(
 
 CREATE TABLE IF NOT EXISTS event(
     id SERIAL PRIMARY KEY,
-    name TEXT DEFAULT 'DEFAULT name' NOT NULL,
+    title TEXT DEFAULT 'Adega Leonor Party' NOT NULL,
     description TEXT DEFAULT('FEUP party') NOT NULL,
     start_date TIMESTAMP DEFAULT (
         to_timestamp('05 Dec 2023 22:00', 'DD Mon YYYY HH24:MI')
@@ -89,7 +95,8 @@ CREATE TABLE IF NOT EXISTS report(
     reporter_id uuid,
     admin_id INT,
     report_text TEXT NOT NULL,
-		report_date TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
+        report_date TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
+        report_type REPORT_TYPE,
     report_status REPORT_STATUS,
     FOREIGN KEY (reported_id) REFERENCES authorized_user(id),
     FOREIGN KEY (reporter_id) REFERENCES authorized_user(id),
@@ -119,19 +126,11 @@ CREATE TABLE IF NOT EXISTS comments(
     event_id INT,
     parent_comment_id INT DEFAULT NULL, -- null if a new comment and comment_id of the parent if a reply
     comment_date DATE DEFAULT (current_date) CHECK (comment_date <= current_date),
-		FOREIGN KEY (user_id, event_id) REFERENCES user_event (user_id, event_id),     -- double reference 
-		FOREIGN KEY (parent_comment_id) REFERENCES comments(id)
+        FOREIGN KEY (user_id, event_id) REFERENCES user_event (user_id, event_id),     -- double reference 
+        FOREIGN KEY (parent_comment_id) REFERENCES comments(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS notification(
-    id SERIAL PRIMARY KEY,
-		user_id uuid,
-		notification_type type_notification NOT NULL,
-    notification_text TEXT NOT NULL DEFAULT ('text'),
-    notification_date DATE NOT NULL DEFAULT (current_date) CHECK (notification_date <= current_date),
-    FOREIGN KEY (user_id) REFERENCES authorized_user(id)
-);
 
 
 CREATE TABLE IF NOT EXISTS tag(
@@ -154,30 +153,68 @@ CREATE TABLE IF NOT EXISTS photo(
 
 CREATE TABLE IF NOT EXISTS poll(
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
+    event_id INT NOT NULL,
+    question TEXT DEFAULT('What is your favorite programming language?') NOT NULL,
     starts_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP check(starts_at <= CURRENT_TIMESTAMP) NOT NULL,
     end_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP + INTERVAL '1 DAY') check(end_at > starts_at) NOT NULL,
-    event_id INT,
     FOREIGN KEY (event_id) REFERENCES event(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS poll_option(
+CREATE TABLE IF NOT EXISTS poll_choice(
     id SERIAL PRIMARY KEY,
-    option TEXT NOT NULL,
     poll_id INT,
+    choice TEXT NOT NULL,
+        UNIQUE(poll_id, id),
     FOREIGN KEY (poll_id) REFERENCES poll(id)
 );
 
 
 CREATE TABLE IF NOT EXISTS poll_vote(
-    vote_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id uuid,
-		event_id INT,
-    option_id INT,
-    date DATE NOT NULL,
-    FOREIGN KEY (vote_id) REFERENCES poll(id),
+    event_id INT,
+    poll_id INT,
+    choice_id INT,
+    date TIMESTAMP DEFAULT(CURRENT_TIMESTAMP) NOT NULL,
+    UNIQUE(poll_id, user_id),
+    UNIQUE(choice_id, poll_id),
     FOREIGN KEY (user_id, event_id) REFERENCES user_event (user_id, event_id),     -- double reference 
-    FOREIGN KEY (option_id) REFERENCES poll_option(id)
+    FOREIGN KEY (choice_id, poll_id) REFERENCES poll_choice(id, poll_id)           -- double reference
+);
+
+
+CREATE TABLE IF NOT EXISTS notification(
+    id SERIAL PRIMARY KEY,
+    user_id uuid,
+    notification_type type_notification NOT NULL,
+    notification_title TEXT NOT NULL DEFAULT ('Main topic of the notification (header)'),
+    body TEXT,  -- is not supposed to be filled for all types of notifications
+    notification_date DATE NOT NULL DEFAULT (current_date) CHECK (notification_date <= current_date),
+    FOREIGN KEY (user_id) REFERENCES authorized_user(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS comment_notification(
+		id      INTEGER NOT NULL REFERENCES notification (id) ON DELETE CASCADE,
+    comment INTEGER NOT NULL REFERENCES comments (id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS poll_notification
+(
+    id      INTEGER NOT NULL REFERENCES notification (id) ON DELETE CASCADE,
+    poll 		INTEGER NOT NULL REFERENCES poll (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS event_notification
+(
+    id      INTEGER NOT NULL REFERENCES notification (id) ON DELETE CASCADE,
+    event 	INTEGER NOT NULL REFERENCES event (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS report_notification
+(
+    id     INTEGER NOT NULL REFERENCES notification (id) ON DELETE CASCADE,
+    report INTEGER NOT NULL REFERENCES report (id) ON DELETE CASCADE
 );
