89007fa3fd0645812f639ac33510c3d6ce561a08
David <davidburchakov@gmail.com>
2022-12-27 22:20:55 +0000

    faqs simplified and fixed

--- resources/views/pages/faqs.blade.php
+++ resources/views/pages/faqs.blade.php
@@ -2,17 +2,31 @@
 
 @section('content')
 
-<h1>{{$heading}}</h1>
+
+<style>
+    .gray{
+     position: relative;
+     padding: 50px;
+     text-align: center;
+     background-color: rgba(0, 0, 0, 0.8);
+     color: white;       
+   }
+</style>
+
+
+<div class="gray">
+<h1><b>{{$heading}}</b></h1>
 
 @unless (count($faqs) == 0)
     
 
 @foreach($faqs as $faq)
-<h2>{{$faq['q']}}</h2>
-<p>{{$faq['a']}}</p>
-
+<div style="padding: 15px">
+<h3>Q: {{$faq['q']}}</h3>
+<p>A: {{$faq['a']}}</p>
+</div>
 @endforeach
-
+</div>
 
 @else
 
