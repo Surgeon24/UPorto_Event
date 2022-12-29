142eb72acf587008ca8e46f37c33b2b25cc1f00c
Mikhail <ermol24@icloud.com>
2022-12-29 16:36:55 +0000

    scout was removed

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,12 +5,10 @@ namespace App\Models;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Laravel\Scout\Searchable;
 
 class Event extends Model
 {
     // use HasFactory;
-    use Searchable;
     public $timestamps = false;
 
     protected $table = 'event';
