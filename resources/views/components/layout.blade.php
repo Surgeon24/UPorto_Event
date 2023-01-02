7c9a0e801ee5479d7a0c9bbca9117384b51f5740
Mikhail <ermol24@icloud.com>
2023-01-02 22:13:04 +0000

    user search

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -108,6 +108,7 @@
         <li><a id="len2" class="hoverable" href="{{ url('/about') }}" style="color: rgb(164, 224, 243);">About</a></li>
         <li><a id="len3" class="hoverable" href="{{ url('/all_events') }}" style="color: rgb(164, 224, 243);">Browse events</a></li>
         <li><a id="len3" class="hoverable" href="{{ url('/my_events') }}" style="color: rgb(164, 224, 243);">My events</a></li>
+        <li><a id="len3" class="hoverable" href="{{ url('/all_users') }}" style="color: rgb(240, 123, 123);">Search users</a></li>
         <li><a id="len4" class="hoverable" href="{{ url('/faqs') }}" style="color: rgb(164, 224, 243);">FAQ</a></li>
 
         @if (Auth::check())
---
+++ resources/views/pages/all_users.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+
+    @include('partials.user_list', ['users' => $user])
+
+</x-layout>
