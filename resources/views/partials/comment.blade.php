e5ab9ce5b36c5d1124f9d4b090c31e934f0fb0c2
Mikhail <ermol24@icloud.com>
2023-01-03 21:21:25 +0000

    fix

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -58,6 +58,7 @@
 @php
     $user= \App\Models\User::where('id', $comment->user_id)->first();
     $role= \App\Models\User_event::where('event_id', $comment->event_id)->where('user_id', $user->id)->first()->role;
+    $numberOfVotes = \App\Models\CommentVote::where('comment_id', $comment->id)->get()->count();
 @endphp
 
 <p></p>
@@ -104,7 +105,7 @@
         <div class="like">
         <button  data-id="{{$comment->id}}" style="background-color:rgb(23, 95, 219)">
             <i class="fa fa-thumbs-up"></i>
-            <span class="icon" style="color:rgb(248, 248, 248)">Like</span>
+            <span class="icon" style="color:rgb(248, 248, 248)">Like {{$numberOfVotes}}</span>
         </button>
         </div>
     </td>
