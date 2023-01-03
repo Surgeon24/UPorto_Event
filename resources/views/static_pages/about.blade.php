866a0bd0a033719d3c0e098f3995841897dcc298
David <davidburchakov@gmail.com>
2023-01-03 06:08:44 +0000

    about page filled

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -14,16 +14,14 @@
     <div class="column">
       <div class="card">
         
-        <img src="{{asset('images/david.jpg')}}" alt="David" style="width:100%">
-    
-
-        
+        {{-- 350x450 --}}
+        <img src="{{asset('images/david.jpg')}}" alt="David" style="width:100%">     
           <div class="gray">
           <h2>David Burchakov</h2>
           <p class="title">FEUP</p>
           <p>Erasmus student</p>
           <p>up202203777@fe.up.pt</p>
-          <p><button class="button">Contact</button></p>
+          <p><button class="button" onclick="window.location.href='https://www.facebook.com/spleentery/';" >Contact</button></p>
           </div>
       </div>
     </div>
@@ -37,33 +35,33 @@
           <p class="title">FEUP</p>
           <p>Erasmus student</p>
           <p>up202203498@fe.up.pt</p>
-          <p><button class="button">Contact</button></p>
+          <p><button class="button" onclick="window.location.href='https://www.facebook.com/profile.php?id=100009885852706';">Contact</button></p>
         </div>
       </div>
     </div>
     
     <div class="column">
         <div class="card">
-          <img src="{{asset('images/no-image.jpeg')}}" alt="Mike" style="width:100%">
+          <img src="{{asset('images/valter.jpeg')}}" alt="Valter" style="width:100%">
           <div class="gray">
             <h2>Valter Castro</h2>
             <p class="title">FEUP</p>
-            <p></p>
-            <p>up...@fe.up.pt</p>
-            <p><button class="button">Contact</button></p>
+            <p>student</p>
+            <p>up201706546@edu.fe.up.pt</p>
+            <p><button class="button" onclick="window.location.href='https://www.facebook.com/search/top?q=valter%20castro';" >Contact</button></p>
           </div>
         </div>
       </div>
 
       <div class="column">
         <div class="card">
-          <img src="{{asset('images/no-image.jpeg')}}" alt="Mike" style="width:100%">
+          <img src="{{asset('images/jao.jpg')}}" alt="Jao" style="width:100%">
           <div class="gray">
-            <h2>Joao</h2>
+            <h2>Joao de Sousa</h2>
             <p class="title">FEUP</p>
-            <p></p>
-            <p>up...@fe.up.pt</p>
-            <p><button class="button">Contact</button></p>
+            <p>student</p>
+            <p>up201904739@edu.fc.up.pt</p>
+            <p><button class="button" onclick="window.location.href='https://www.facebook.com/search/top?q=joao%20bernardo%20pereira%20de%20sousa';">Contact</button></p>
           </div>
         </div>
       </div>
